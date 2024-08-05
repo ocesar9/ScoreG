f63013a13076b761156d6e6b29a2a5e97c1f63ac
LucasMonteiro10 <lucasmonteirovilela@hotmail.com>
2024-08-05 11:41:08 -0300

    Adiciona variáveis restantes à Data Class Game

--- app/src/main/java/com/example/scoreg/DB/Game.kt
+++ app/src/main/java/com/example/scoreg/DB/Game.kt
@@ -4,5 +4,7 @@ data class Game(
     val description: String = "",
     val release_year: Int = 0,
     val score: Int = 0,
-    val title: String = ""
+    val title: String = "",
+    val urlImage: String = "",
+    val urlTrailer: String = ""
 )
