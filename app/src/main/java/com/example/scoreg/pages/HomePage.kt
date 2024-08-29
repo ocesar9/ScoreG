f8bc0ace63f1a43b3974f3c96182093fd75c7a1f
ocesar9 <juliocesaoguimaraes@gmail.com>
2024-08-29 20:48:51 -0300

    Create Logout TopBar Button

--- app/src/main/java/com/example/scoreg/pages/HomePage.kt
+++ app/src/main/java/com/example/scoreg/pages/HomePage.kt
@@ -1,24 +1,37 @@
 package com.example.scoreg.pages
 
 
+import android.app.Activity
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.scoreg.components.Navbar
 import com.example.scoreg.models.MainViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.weatherapp.utils.IntentUtils
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomePage(navController: NavController, modifier: Modifier = Modifier) {
     val viewModel = viewModel<MainViewModel>();
@@ -26,10 +39,32 @@ fun HomePage(navController: NavController, modifier: Modifier = Modifier) {
     val games by viewModel.games.collectAsState();
     val isSearching by viewModel.isSearching.collectAsState();
 
+    val context = LocalContext.current // Obtenha o contexto
+
+
     Column(
         modifier = modifier
             .fillMaxSize()
+
     ) {
+        TopAppBar(
+            title = { /*TODO*/ },
+            actions = {
+                IconButton(
+                    onClick = {
+                        Firebase.auth.signOut()
+                        val intent = IntentUtils.createLoginActivityIntent(context)
+                        context.startActivity(intent)
+                    }
+                ) {
+
+                    Icon(
+                        imageVector = Icons.Default.ExitToApp,
+                        contentDescription = "Localized description"
+                    )
+                }
+            }
+        )
         Navbar(navController = navController)
 
         Spacer(modifier = Modifier.height(16.dp))
