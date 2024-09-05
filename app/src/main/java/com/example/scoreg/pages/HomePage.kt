8a059f45e945bf1702635ea0be0e400b4afec31d
ocesar9 <juliocesaoguimaraes@gmail.com>
2024-09-04 21:18:44 -0300

    wip:Layout HomePage refactoring

--- app/src/main/java/com/example/scoreg/pages/HomePage.kt
+++ app/src/main/java/com/example/scoreg/pages/HomePage.kt
@@ -15,9 +15,11 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -42,6 +44,10 @@ fun HomePage(navController: NavController, mainViewModel: MainViewModel, modifie
     ) {
         TopAppBar(
             title = { /*TODO*/ },
+            modifier = Modifier.height(56.dp),
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color(0xFF25F396)
+            ),
             actions = {
                 IconButton(
                     onClick = {
